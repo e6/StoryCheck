cfe96ec492de948fbf273cbf4f614bb66a2c425d
Nick Hargreaves <hargreaves1234@gmail.com>
2015-10-27 17:09:04 +0300

    fancy category picker starter using sweetsheet

---
+++ app/src/main/java/org/codeforafrica/storycheck/MainActivity.java
@@ -0,0 +1,73 @@
+package org.codeforafrica.storycheck;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.mingle.entity.MenuEntity;
+import com.mingle.sweetpick.DimEffect;
+import com.mingle.sweetpick.SweetSheet;
+import com.mingle.sweetpick.ViewPagerDelegate;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SweetSheet mReportCategoriesSheet;
+    private RelativeLayout rl;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        rl = (RelativeLayout) findViewById(R.id.rl);
+        setupViewpager();
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mReportCategoriesSheet.toggle();
+            }
+        });
+
+    }
+
+    private void setupViewpager() {
+
+
+        mReportCategoriesSheet = new SweetSheet(rl);
+
+        mReportCategoriesSheet.setMenuList(R.menu.menu_sweet);
+        mReportCategoriesSheet.setDelegate(new ViewPagerDelegate());
+        mReportCategoriesSheet.setBackgroundEffect(new DimEffect(0.5f));
+        mReportCategoriesSheet.setOnMenuItemClickListener(new SweetSheet.OnMenuItemClickListener() {
+            @Override
+            public boolean onItemClick(int position, MenuEntity menuEntity1) {
+
+                Toast.makeText(MainActivity.this, menuEntity1.title + "  " + position, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+
+        if (mReportCategoriesSheet.isShow()) {
+                mReportCategoriesSheet.dismiss();
+        } else {
+            super.onBackPressed();
+        }
+
+
+    }
+
+
+}
