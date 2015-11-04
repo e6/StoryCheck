fe1e66dc59c3c436781b7ad052bc8ae2ef0b17d1
Nick Hargreaves <hargreaves1234@gmail.com>
2015-11-04 19:07:43 +0300

    custom swipe touch listener

---
+++ app/src/main/java/org/codeforafrica/storycheck/helpers/OnSwipeTouchListener.java
@@ -0,0 +1,30 @@
+package org.codeforafrica.storycheck.helpers;
+
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ListView;
+
+public class OnSwipeTouchListener implements View.OnTouchListener {
+
+    ListView list;
+    private GestureDetector gestureDetector;
+    private Context context;
+
+    public OnSwipeTouchListener(Context ctx, ListView list) {
+        gestureDetector = new GestureDetector(ctx, new GestureListener(list));
+        context = ctx;
+        this.list = list;
+    }
+
+    public OnSwipeTouchListener() {
+        super();
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+    }
+    
+}
