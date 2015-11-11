3330610320dd42d31b17820910976ee470b55bbc
Nick Hargreaves <hargreaves1234@gmail.com>
2015-11-11 13:49:35 +0300

    link to dummy api

--- app/src/main/java/org/codeforafrica/storycheck/GlobalConstants.java
+++ app/src/main/java/org/codeforafrica/storycheck/GlobalConstants.java
@@ -3,6 +3,6 @@ package org.codeforafrica.storycheck;
 public class GlobalConstants {
 
     public static String TAG = "StoryCheck";
-    public static String CHECKLISTS_API = "";
+    public static String CHECKLISTS_API = "https://f9637f21522185f0ad69fcbd2c395c82fd76e82c.googledrive.com/host/0B3A9AvgQXoQwbXB3QmsxOGF0OWc/checklists.json";
 
 }
