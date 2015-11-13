55950ca6e66dc6724ab42dcb122cf71a99aaabac
Nick Hargreaves <hargreaves1234@gmail.com>
2015-11-13 10:21:54 +0300

    add tab indicator to viewpager

---
+++ app/src/main/java/org/codeforafrica/storycheck/PageViewActivity.java
@@ -0,0 +1,47 @@
+package org.codeforafrica.storycheck;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PageViewActivity extends FragmentActivity {
+    MyPageAdapter pageAdapter;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_view);
+        List<Fragment> fragments = getFragments();
+        pageAdapter = new MyPageAdapter(getSupportFragmentManager(), fragments);
+        ViewPager pager =
+                (ViewPager)findViewById(R.id.viewpager);
+        pager.setAdapter(pageAdapter);
+    }
+    private List<Fragment> getFragments() {
+        List<Fragment> fList = new ArrayList<Fragment>();
+        fList.add(MyFragment.newInstance("Fragment 1"));
+        fList.add(MyFragment.newInstance("Fragment 2"));
+        fList.add(MyFragment.newInstance("Fragment 3"));
+        return fList;
+    }
+    class MyPageAdapter extends FragmentPagerAdapter {
+        private List<Fragment> fragments;
+        public MyPageAdapter(FragmentManager fm, List<Fragment> fragments) {
+            super(fm);
+            this.fragments = fragments;
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return this.fragments.get(position);
+        }
+        @Override
+        public int getCount() {
+            return this.fragments.size();
+        }
+    }
+}
